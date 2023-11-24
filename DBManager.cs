505cbc8c64efc8e1a2044fb7671e3cd8ee3f7aa3
Valery <v.tat555@gmail.com>
2023-11-24 18:56:04 +0400

    meow

--- DBManager.cs
+++ DBManager.cs
@@ -3,8 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Runtime.Remoting.Contexts;
-using System.Data.Entity;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookShopWPF
 {
